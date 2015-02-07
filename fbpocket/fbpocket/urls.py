ef6f881ae359ab2d6cf4381133dcc9a3c03099d2
unknown <cymn_td@yahoo.com>
2015-02-07 15:00:12 +0800

    Setup Done

--- fbpocket/fbpocket/urls.py
+++ fbpocket/fbpocket/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns, include, url
 from pocketlist import views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+	(r'^admin/', include(admin.site.urls)),
 	(r'^$', views.index),
+	
 )
