75d20a3491b3ae3879cfa6bdecdf38314971a14b
unknown <cymn_td@yahoo.com>
2015-02-07 14:16:41 +0800

    'setup'

--- fbpocket/pocketlist/views.py
+++ fbpocket/pocketlist/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.shortcuts import render, render_to_response, redirect
+from django.contrib.auth.decorators import login_required 	#for login required
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.template import RequestContext
+from django.http import HttpResponseRedirect,HttpResponse
+from django.core.urlresolvers import reverse
+from pocketlist.models import *
+
+def index(request):
+	param = {}
+	return render_to_response(
+				'index.html',
+				param,
+				context_instance=RequestContext(request)
+	)
